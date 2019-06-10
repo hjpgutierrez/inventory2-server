8cd0703af2f5391384747039d20d46fe1977ef48
Pablo <montielguilabert@gmail.com>
2019-06-10 12:50:34 +0200

    Commit: CRUD Proveedores

--- src/models/proveedores.js
+++ src/models/proveedores.js
@@ -9,8 +9,7 @@ module.exports = class Proveedores {
         return new Promise(function(resolve, reject) {
             var connection = require('./database');
 
-            var query_str =
-                "SELECT nombre,nit,direccion,telefono,contacto,cod_user_alta,fecha_alta FROM inv_proveedor ORDER BY cod ASC;";
+            var query_str = "SELECT * FROM inv_proveedor ORDER BY cod ASC;";
 
             var query_var = [];
 
@@ -25,28 +24,66 @@ module.exports = class Proveedores {
         });
     }
 
-    altaProveedor(nombre, nit, direccion, telefono, email, contacto, codusuario) {
+    insertarProveedor(nombre, nit, direccion, telefono, email, contacto, codusuario) {
         return new Promise(function(resolve, reject) {
             var connection = require('./database');
 
-            var query_str =
-                "INSERT INTO inv_proveedor (nombre,nit,direccion,telefono,email,contacto,cod_user_alta,fecha_alta) values (?,?,?,?,?,?,?,NOW());";
+            var query_str = "INSERT INTO inv_proveedor (nombre,nit,direccion,telefono,email,contacto,cod_user_alta,fecha_alta) values (?,?,?,?,?,?,?,NOW());";
 
             var query_var = [nombre, nit, direccion, telefono, email, contacto, codusuario];
 
-            connection.query(query_str, query_var, function(err, rows) {
+            connection.query(query_str, query_var, function(err, results) {
 
                 if (err) {
                     return reject(err);
                 }
 
-                resolve(rows);
+                resolve(results.affectedRows);
+
             });
         });
-
-
     }
 
+    actualizarProveedor(nombre, nit, direccion, telefono, email, contacto, codusermod, cod) {
+        return new Promise(function(resolve, reject) {
+
+            var connection = require("./database");
+
+            var query_str = "UPDATE inv_proveedor SET nombre = ?, nit = ?, direccion = ?, telefono = ?, email = ?, contacto = ?, cod_user_mod = ?, fecha_mod = NOW() WHERE cod = ?;";
+
+            var query_var = [nombre, nit, direccion, telefono, email, contacto, codusermod, cod];
+
+            connection.query(query_str, query_var, function(err, results) {
+
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve(results.affectedRows);
+            });
+        });
+    }
+
+    borrarProveedor(cod) {
+        return new Promise(function(resolve, reject) {
+
+            var connection = require("./database");
+
+            var query_str = "DELETE FROM inv_proveedor WHERE cod = ?;";
+
+            var query_var = [cod];
+
+            connection.query(query_str, query_var, function(err, results) {
+
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve(results.affectedRows);
+
+            });
+        });
+    }
 
 
 
--- src/routes/proveedores.js
+++ src/routes/proveedores.js
@@ -6,57 +6,29 @@ var Proveedores = require('../models/proveedores');
 var mdAutenticacion = require('../middlewares/autenticacion');
 
 
-// cargarProveedores
-
 router.get('/', mdAutenticacion.verificaToken, (req, res) => {
 
     var proveedores = new Proveedores();
 
     proveedores.cargarProveedores()
-
-    .then(function(dato) {
-
-        if (dato && dato.length > 0) {
-            console.log(dato);
-            return res.status(200).json({
-                ok: true,
-                resp: dato
-            });
-
-        } else {
-            return res.status(200).json({
-                ok: false,
-                mensaje: 'No se encontraron registros'
-            });
-        }
-
-    }).catch(function(err) {
-        return res.status(500).json({
-            ok: false,
-            mensaje: 'Error interno'
-        });
-    });
-
-});
-
-
-router.post('/alta_proveedor', mdAutenticacion.verificaToken, (req, res) => {
-
-    const { nombre, nit, direccion, telefono, email, contacto } = req.body;
-    let codusuario = req.usuario.cod;
-
-    var proveedor = new Proveedores();
-
-    proveedor.altaProveedor(nombre, nit, direccion, telefono, email, contacto, codusuario)
         .then(function(dato) {
 
-
             if (dato && dato.length > 0) {
-                console.log(dato);
+
+                let lista = dato.map((fila) => {
+                    return {
+                        cod: fila.cod,
+                        nombre: fila.nombre,
+                        nit: fila.nit,
+                        direccion: fila.direccion,
+                        telefono: fila.telefono,
+                        contacto: fila.contacto,
+                    }
+                });
 
                 return res.status(200).json({
                     ok: true,
-                    resp: dato
+                    resp: lista
                 });
 
             } else {
@@ -75,11 +47,102 @@ router.post('/alta_proveedor', mdAutenticacion.verificaToken, (req, res) => {
 
 });
 
+router.post('/insertarproveedor', mdAutenticacion.verificaToken, (req, res) => {
+
+    const { nombre, nit, direccion, telefono, email, contacto } = req.body;
+
+    let codusuario = req.usuario.cod;
+
+    var proveedor = new Proveedores();
+
+    proveedor.insertarProveedor(nombre, nit, direccion, telefono, email, contacto, codusuario)
+        .then(function(dato) {
+
+            if (dato > 0) {
+                return res.status(200).json({
+                    ok: true,
+                    resp: dato
+                });
+            } else {
+                return res.status(200).json({
+                    ok: false,
+                    mensaje: 'No se inserto el proveedor'
+                });
+            }
+
+        }).catch(function(err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error interno'
+            });
+        });
+});
 
 
+router.put('/actualizarproveedor', mdAutenticacion.verificaToken, (req, res) => {
 
+    const { nombre, nit, direccion, telefono, email, contacto, cod } = req.body;
 
+    let codusermod = req.usuario.cod;
 
+    var proveedor = new Proveedores();
 
+    proveedor.actualizarProveedor(nombre, nit, direccion, telefono, email, contacto, codusermod, cod)
+
+    .then(function(dato) {
+
+        if (dato > 0) {
+            return res.status(200).json({
+                ok: true,
+                resp: dato
+            });
+
+        } else {
+            return res.status(200).json({
+                ok: false,
+                mensaje: 'No se puedo actualizar el proveedor'
+            });
+        }
+
+    }).catch(function(err) {
+        return res.status(500).json({
+            ok: false,
+            mensaje: 'Error interno'
+        });
+    });
+
+});
+
+router.delete('/borrarproveedor', mdAutenticacion.verificaToken, (req, res) => {
+
+    const { cod } = req.body;
+
+    var proveedor = new Proveedores();
+
+    proveedor.borrarProveedor(cod)
+
+    .then(function(dato) {
+
+        if (dato > 0) {
+
+            return res.status(200).json({
+                ok: true,
+                resp: dato
+            });
+
+        } else {
+            return res.status(200).json({
+                ok: false,
+                mensaje: 'No se encontró el proveedor'
+            });
+        }
+
+    }).catch(function(err) {
+        return res.status(500).json({
+            ok: false,
+            mensaje: 'Error interno'
+        });
+    });
+});
 
 module.exports = router;
